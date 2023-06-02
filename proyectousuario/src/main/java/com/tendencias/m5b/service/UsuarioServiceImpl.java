f009acbff7a357da4098362b5e1a6b7a2251f3fa
lilySesh <liliana.cochancela.est@tecazuay.edu.ec>
2023-06-01 22:53:01 -0500

    subido

---
+++ proyectousuario/src/main/java/com/tendencias/m5b/service/UsuarioServiceImpl.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tendencias.m5b.service;
+
+
+import com.tendencias.m5b.model.Usuario;
+import com.tendencias.m5b.repositorio.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author 59398
+ */
+@Service
+public class UsuarioServiceImpl extends GenericServiceImpl<Usuario, Integer> implements GenericService<Usuario, Integer> {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public CrudRepository<Usuario, Integer> getDao() {
+        return usuarioRepository;
+    }
+
+
+}
